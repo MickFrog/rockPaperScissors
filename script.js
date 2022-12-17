2c7062c6e2a8bd34c79368d4b617ca6198b7918e
MickFrog <107855646+MickFrog@users.noreply.github.com>
2022-12-17 12:32:46 +0300

    Work on user input and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+//Get case insensitive user input
+let UserChoice = prompt("Rock Paper Scissors");
+
+function capitalise(capWord) {
+    let newWord = capWord.charAt(0).toUpperCase() + capWord.slice(1).toLowerCase();
+    return newWord;
+}
+
+let playerChoice = capitalise(UserChoice);
+console.log(playerChoice);
+
+//Random Computer Choice
+const choices = ["Rock", "Paper", "Scissors"];
+
+let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+
+console.log(computerChoice);
