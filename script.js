c55493d3e7f1597760c444530a6a51d83e8acf1e
MickFrog <107855646+MickFrog@users.noreply.github.com>
2022-12-17 13:03:49 +0300

    Write code for processing round

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ function capitalise(capWord) {
 }
 
 let playerChoice = capitalise(prompt("Rock Paper Scissors"));
-console.log(playerChoice);
+// console.log(playerChoice);
 
 //Random Computer Choice
 const choices = ["Rock", "Paper", "Scissors"];
 
 let computerChoice = choices[Math.floor(Math.random()*choices.length)];
 
-console.log(computerChoice);
+// console.log(computerChoice);
+
+//Process game round against rules
+if(playerChoice == computerChoice) {
+    console.log("You draw. Please Refresh to play again.");
+} else if((playerChoice == "Rock" && computerChoice == "Scissors") 
+    || (playerChoice == "Paper" && computerChoice == "Rock") 
+    || (playerChoice == "Scissors" && computerChoice == "Paper")) {
+
+console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
+} else {
+
+    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+}
