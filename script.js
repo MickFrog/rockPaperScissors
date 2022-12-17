f65eb63f7d8b64c8489841af2a2f860968a96ae5
MickFrog <107855646+MickFrog@users.noreply.github.com>
2022-12-17 13:38:45 +0300

    Add 5-round capability

--- script.js
+++ script.js
@@ -4,25 +4,71 @@ function capitalise(capWord) {
     return newWord;
 }
 
-let playerChoice = capitalise(prompt("Rock Paper Scissors"));
-// console.log(playerChoice);
+function getUserChoice(){
+    return capitalise(prompt("Rock Paper Scissors"));
+}
 
 //Random Computer Choice
 const choices = ["Rock", "Paper", "Scissors"];
 
-let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+function getComputerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
 
-// console.log(computerChoice);
+function validateUserChoice(userChoice) {
+    for(let i = 0; i < choices.length; i++) {
+        if(userChoice == choices[i]){
+            return true;
+        }
+    }
+
+    return false;
+}
 
 //Process game round against rules
-if(playerChoice == computerChoice) {
-    console.log("You draw. Please Refresh to play again.");
-} else if((playerChoice == "Rock" && computerChoice == "Scissors") 
-    || (playerChoice == "Paper" && computerChoice == "Rock") 
-    || (playerChoice == "Scissors" && computerChoice == "Paper")) {
+function playRound(playerChoice, computerChoice) {
+    //validate input
+    if(!validateUserChoice(playerChoice)) {
+        console.log("Invalid input. Point given to computer.")
+        return 0;
+    }
 
-console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-} else {
-
-    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    if(playerChoice == computerChoice) {
+        console.log("You draw.");
+        return 2;
+    } else if((playerChoice == "Rock" && computerChoice == "Scissors") 
+        || (playerChoice == "Paper" && computerChoice == "Rock") 
+        || (playerChoice == "Scissors" && computerChoice == "Paper")) {
+    
+    console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
+    return 1;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+        return 0;
+    }
 }
+
+function game(){
+    let userScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let result = playRound(getUserChoice(), getComputerChoice());
+
+        if(result == 1) {
+            userScore++;
+        } else if(result == 0) {
+            computerScore++;
+        }
+    }
+    
+    if(userScore == computerScore) {
+        console.log("WHAT A TOUGH GAME. IT WAS A DRAW");
+    } else if(userScore > computerScore) {
+        console.log("YOU ARE THE OVERALL WINNER");
+    } else {
+        console.log("YOU LOST THE GAME.");
+    }
+}
+
+game();
